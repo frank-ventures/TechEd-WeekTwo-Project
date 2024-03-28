0c5ada4a5fcc05f8f815b54e83e61211b764dfaa
frank-ventures <6458936+frank-ventures@users.noreply.github.com>
2024-03-28 14:27:41 +0000

    basic working implementation

--- README.md
+++ README.md
@@ -20,6 +20,20 @@ I kept track of my thoughts and tasks here: https://frankjs.notion.site/Day-Nine
 
 ### What went well
 
+Getting the initial JavaScript code to work didn't take long thanks to the lessons and practice from this week.
+
+I was really pleased with how the counters saved and loaded in and out of local storage, and that the counter reset worked very simply.
+
 ### Sticky points
 
 ## Further Detail
+
+### A Simple Start
+
+I started with a very basic page with very little UI aesthetics! This allowed me to focus on the JS code.
+
+I kept my basic JS code easy-to-read, plainly naming the buttons and functions. this allowed me to follow the trail of execution after a 'button press'.
+
+Using a little CSS let me visually seperate the page.
+
+I realised that loading the User Stats on page load could probably be done as a "for loop" for an object. This will be a stretch implementation.
--- app.js
+++ app.js
@@ -2,21 +2,69 @@
 // --Buttons
 const coffeeButton = document.getElementById("coffeeButton");
 const upgradeButton = document.getElementById("upgradeButton");
+const resetButton = document.getElementById("resetButton");
 // --Page Display
 const beanCounter = document.getElementById("beanCounter");
 const beansPerSecond = document.getElementById("beansPerSecond");
-//  --User Stats Object
+//  --Set the User Stats Object
 const userStats = {
   beanCount: 0,
   beansPerSecond: 0,
 };
 
+// Runs once on page load to check if a user has locally stored stats, then updates the live stats to match.
+const storedUserStats = JSON.parse(localStorage.getItem("userStats"));
+
+if (storedUserStats) {
+  // I will bring in a 'for loop' later, once I understand how to implement it, to update the object.
+  //   for (let key in storedUserStats) {
+  //     if (storedUserStats.hasOwnProperty(key)) {
+  //       const element = object[key];
+  //     }
+  //   }
+  userStats.beanCount = storedUserStats.beanCount;
+  userStats.beansPerSecond = storedUserStats.beansPerSecond;
+
+  updatePageStats();
+}
+
 // functions
 function increaseBeanCount() {
   userStats.beanCount++;
-  console.log(userStats.beanCount);
+  updatePageStats();
+  updateUserStorage();
 }
+
+function getUpgrade() {
+  userStats.beanCount--;
+  userStats.beansPerSecond++;
+  updatePageStats();
+  updateUserStorage();
+}
+
+function updatePageStats() {
+  beanCounter.textContent = userStats.beanCount;
+  beansPerSecond.textContent = userStats.beansPerSecond;
+}
+
+function updateUserStorage() {
+  localStorage.setItem("userStats", JSON.stringify(userStats));
+}
+
+function resetUserStats() {
+  userStats.beanCount = 0;
+  userStats.beansPerSecond = 0;
+  updatePageStats();
+  updateUserStorage();
+}
+
+setInterval(() => {
+  userStats.beanCount = userStats.beanCount + userStats.beansPerSecond;
+  updatePageStats();
+  updateUserStorage();
+}, 1000);
 
 // event listeners
 coffeeButton.addEventListener("click", increaseBeanCount);
 upgradeButton.addEventListener("click", getUpgrade);
+resetButton.addEventListener("click", resetUserStats);
